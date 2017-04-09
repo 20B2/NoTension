6b1188d412a7e5abb663375433fcd35409a0976b
Binod Marikhu <bnod_468@hotmail.com>
2017-04-09 14:08:51 +0545

    Log In and Log off changes

--- WebApplication/Controllers/AccountController.cs
+++ WebApplication/Controllers/AccountController.cs
@@ -174,7 +174,8 @@ namespace WebApplication.Controllers
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation(4, "User logged out.");
-            return RedirectToAction(nameof(HomeController.Index), "Home");
+            // return RedirectToAction(nameof(HomeController.Index), "Home");
+            return RedirectToAction(nameof(Login));
         }
 
         [HttpPost]
