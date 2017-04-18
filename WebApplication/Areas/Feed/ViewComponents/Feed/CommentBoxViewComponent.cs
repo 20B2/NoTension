a89450cb927e40a26af113d101c8135bdc97f840
davidsilwal <de.davidsilwal@gmail.com>
2017-04-18 12:16:46 +0545

    some changes

---
+++ WebApplication/Areas/Feed/ViewComponents/Feed/CommentBoxViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.Areas.Feed.ViewComponents.Feed
+{
+    [ViewComponent(Name ="CommentBox")]
+    public class CommentBoxViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication/Helpers/SeedDataHelper.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.Core.Domains;
+using WebApplication.Core.Domains.MessageTemplate;
+using WebApplication.Core.Domains.StatusType;
+using WebApplication.Identity;
+using WebApplication.Infrastructure;
+
+namespace WebApplication.Helpers
+{
+    public class SeedDataHelper
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public SeedDataHelper(ApplicationDbContext context, 
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+
+        }
+        public async void Initialize()
+        {
+            var user = new IdentityUser
+            { 
+                Email = $"admin@example.com",
+                UserName = $"admin@example.com",
+                EmailConfirmed = true,
+                LockoutEnabled = false           
+            };
+         
+            if ((_context.RoleCollection.Count("{}") == 0)==true)
+            {
+                var Roles = new List<IdentityRole>()
+                {
+                    new IdentityRole { Name = "Admin" } ,
+                    new IdentityRole { Name = "Registered User" },
+                    new IdentityRole { Name = "Domain Expert" },
+                    new IdentityRole { Name = "Country Manager" },
+                };
+             
+
+                foreach (var role in Roles)
+                {
+                    //await roleStore.CreateAsync(role);
+                    await _roleManager.CreateAsync(role);
+
+                }
+                               
+            }
+
+            if ((_context.UserCollection.Count("{}") == 0) == true)
+            {
+             
+                await _userManager.CreateAsync(user, $"Admin123!@#");
+                await _userManager.AddToRoleAsync(user, "Admin");
+
+            }
+
+            if ((_context.StatusTypeCollection.Count("{}") == 0) == true)
+            {
+                var types = new List<StatusType>()
+                {
+                    new StatusType{  TypeName=$"Psychological Problem"},
+                    new StatusType{  TypeName=$"Health Problem"},
+                    new StatusType{  TypeName=$"Love Problem"},
+                    new StatusType{  TypeName=$"Depressed"},
+                    new StatusType{  TypeName=$"Happy"},
+                    new StatusType{  TypeName=$"Sad"},
+                    new StatusType{  TypeName=$"Angry"}
+                };
+
+                await _context.StatusTypeCollection.InsertManyAsync(types);
+
+            }
+
+            if ((_context.MessageTemplateCollection.Count("{}") == 0) == true)
+            {
+                var messages = new List<MessageTemplate>()
+                {
+                    new MessageTemplate{  MessageTemplateTypeID=$"Welcome", Subject=$"Welcome" },
+                };
+
+                await _context.MessageTemplateCollection.InsertManyAsync(messages);
+            }
+
+
+        }
+
+        
+    }
+}
